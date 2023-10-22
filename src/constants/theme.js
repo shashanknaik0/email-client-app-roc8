18c10f881b4c0b9cd34f2a74944f94f3636f4a07
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-10-22 12:39:21 +0530

    add. const colors

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    accent: '#E54065',
+    background: '#F4F5F9',
+    border:"#CFD2DC",
+    text:"#636363",
+    filterButton:"#E1E4EA",
+    readBackground: "#F2F2F2",   
+}
+
+export { COLORS };
